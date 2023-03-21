72eb931c0fb9381aa32bf6b8239cb39dc8a84025
Vijay Shankar <vijayayush.ak15@gmail.com>
2023-03-21 16:31:33 +0530

    initialized firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: "AIzaSyDqsBDeyPyX4CNiWhmOo4wEOjshCONiI5w",
+    authDomain: "chat-web-app-d3b7a.firebaseapp.com",
+    databaseURL: "https://chat-web-app-d3b7a-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "chat-web-app-d3b7a",
+    storageBucket: "chat-web-app-d3b7a.appspot.com",
+    messagingSenderId: "873416998411",
+    appId: "1:873416998411:web:085449eaa5374fe33b910e"
+  };
+  const app = firebase.initializeApp(config);
+
+  app.name.charAt.call.apply();
+  
+  
