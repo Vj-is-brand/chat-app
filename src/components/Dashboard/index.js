90048214fabf5e4509a1b760c4011cf0f2a0116d
Vijay Shankar <vijayayush.ak15@gmail.com>
2023-04-11 15:23:06 +0530

    linked facebook and google to one account, created provider block

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,50 +1,51 @@
 import React from 'react';
-import  {Drawer,Button, Divider, Alert} from 'rsuite';
+import { Drawer, Button, Divider, Alert } from 'rsuite';
 import { useProfile } from '../../context/profile.contex';
 import { database } from '../../misc/firebase';
 import EditableInput from '../EditableInput';
+import ProviderBlock from './ProviderBlock';
 
-const Dashboard = ({onSignOut}) => {
+const Dashboard = ({ onSignOut }) => {
+  const { profile } = useProfile();
+  const onSave = async newData => {
+    // console.log(newData);
+    const userNickNameRef = database
+      .ref(`/profiles/${profile.uid}`)
+      .child('name');
 
-    const {profile} = useProfile();
-    const onSave = async newData => {
-      // console.log(newData);
-      const userNickNameRef = database.ref(`/profiles/${profile.uid}`).child('name');
-      
-      try {
-        await userNickNameRef.set(newData);
-        Alert.success('Nickname has been updated',4000);
-      } catch (error) {
-        Alert.error(error.message,4000);
-      }
-    };
+    try {
+      await userNickNameRef.set(newData);
+      Alert.success('Nickname has been updated', 4000);
+    } catch (error) {
+      Alert.error(error.message, 4000);
+    }
+  };
 
   return (
     <>
       <Drawer.Header>
-        <Drawer.Title>
-          <h3>Dashboard</h3>
-        </Drawer.Title>
+        <Drawer.Title>Dashboard</Drawer.Title>
       </Drawer.Header>
 
       <Drawer.Body>
-       Hey, {profile.name}
-       <Divider/>
-       <EditableInput
+        <h3>Hey, {profile.name}</h3>
+        <ProviderBlock />
+        <Divider />
+        <EditableInput
           name="Nickname"
           initialValue={profile.name}
           onSave={onSave}
-          lable= {<h6 className='mb-2'>Nickname</h6>}
-       />
+          lable={<h6 className="mb-2">Nickname</h6>}
+        />
       </Drawer.Body>
 
       <Drawer.Footer>
-        <Button block color='red' onClick={onSignOut}>
+        <Button block color="red" onClick={onSignOut}>
           signout
         </Button>
       </Drawer.Footer>
     </>
-  )
-}
+  );
+};
 
 export default Dashboard;
