476036a1808c3f6dd1b3aa548ef2563c017f7d29
Vijay Shankar <vijayayush.ak15@gmail.com>
2023-05-21 09:38:24 +0530

    created chat bottom , send message to chat

--- src/components/chat-window/bottom/index.js
+++ src/components/chat-window/bottom/index.js
@@ -1,7 +1,85 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import { Alert, Avatar, Icon, Input, InputGroup } from 'rsuite';
+import { useParams } from 'react-router';
+import firebase from 'firebase/app';
+import { useProfile } from '../../../context/profile.contex';
+import { database } from '../../../misc/firebase';
+
+function assembleMessage(profile, chatId) {
+  return {
+    roomId: chatId,
+    author: {
+      uid: profile.uid,
+      name: profile.name,
+      createdAt: profile.createdAt,
+      ...(profile.avatar ? { avatar: profile.avatar } : {}),
+    },
+    createdAt: firebase.database.ServerValue.TIMESTAMP,
+  };
+}
 
 const ChatBottom = () => {
-  return <div>bottom</div>;
+  const { chatId } = useParams();
+  const { profile } = useProfile();
+  const [input, setInput] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const onInputChange = useCallback(value => {
+    setInput(value);
+  }, []);
+
+  const onSendClick = async () => {
+    if (input.trim() === '') {
+      return;
+    }
+    
+    const msgData = assembleMessage(profile, chatId);
+    msgData.text=input;
+    const updates = {};
+    
+    const messageId = database.ref('messages').push().key;
+    updates[`messages/${messageId}`] = msgData;
+    updates[`rooms/${chatId}/lastmessage`] = {
+      ...msgData,
+      msgId: messageId,
+    };
+    setIsLoading(true);
+    try {
+      await database.ref().update(updates);
+      setInput('');
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      Alert.error(error.message);
+    }
+  };
+
+  const onKeyDown = (ev) => {
+    if(ev.keyCode === 13){
+      ev.preventDefault();
+      onSendClick();
+    }
+  }
+    
+  return (
+    <div>
+      <InputGroup>
+        <Input
+          placeholder="write a message..."
+          value={input}
+          onChange={onInputChange}
+          onKeyDown={onKeyDown}
+        />
+        <InputGroup.Button
+          color="blue"
+          appearance="primary"
+          onClick={onSendClick}
+          disabled={isLoading}
+        >
+          <Icon icon="send" />
+        </InputGroup.Button>
+      </InputGroup>
+    </div>
+  );
 };
 
 export default ChatBottom;
--- src/components/chat-window/top/index.js
+++ src/components/chat-window/top/index.js
@@ -2,30 +2,34 @@ import React, { memo } from 'react';
 import { Link } from 'react-router-dom';
 import { ButtonToolbar, Icon } from 'rsuite';
 import { useCurrentRoom } from '../../../context/create-room.context';
-import {useMediaQuery} from '../../../misc/custom-hooks';
+import { useMediaQuery } from '../../../misc/custom-hooks';
 import RoomInfoBtnModal from './RoomInfoBtnModal';
 
 const ChatTop = () => {
   const name = useCurrentRoom(v => v.name);
-  const isMobile = useMediaQuery('(max-width:992px)')
+  const isMobile = useMediaQuery('(max-width:992px)');
   return (
     <div>
       <div className="d-flex justify-content-between align-items-center">
-        <h4>
+        <h4 className="text-disappear align-items-center d-flex">
           <Icon
             componentClass={Link}
             to="/"
             icon="arrow-circle-left"
             size="2x"
-            className={isMobile ? 'd-inline-block p-0 mr-2 text-blue link-unstyled': 'd-none' }
+            className={
+              isMobile
+                ? 'd-inline-block p-0 mr-2 text-blue link-unstyled'
+                : 'd-none'
+            }
           />
           <span className="text-disappear">{name}</span>
         </h4>
-        <ButtonToolbar className='ws-nowrap'>todo</ButtonToolbar>
+        <ButtonToolbar className="ws-nowrap">todo</ButtonToolbar>
       </div>
-      <div className='d-flex justify-content-between align-items-center'>
+      <div className="d-flex justify-content-between align-items-center">
         <span>todo</span>
-        <RoomInfoBtnModal/>
+        <RoomInfoBtnModal />
       </div>
     </div>
   );
